891a2bc1e1586f2e3d74d8b8e5b98347a54cc652
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2026-01-09 23:31:38 +0530

    Add user model schema for MongoDB with Mongoose

---
+++ Server/src/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String, required: true
+        },
+        age: {
+            type: Number, required: true
+        },
+        email: {
+            type: String, unique: true,
+        },
+        phone: {
+            type: String, unique: true, required: true
+        },
+        password: {
+            type: String, required: true
+        },
+    },
+    { timestamps: true }
+);
+
+
+const User = mongoose.model("User", userSchema);
+export default User;
